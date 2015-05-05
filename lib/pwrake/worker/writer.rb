6f3ed67dfa314cbcc38161691e96f5fa01a20949
Masahiro Tanaka <tanaka@hpcs.cs.tsukuba.ac.jp>
2015-05-05 23:51:54 +0900

    new method: add_logger, change  prefix from e: to :log, fix @mutex.synchronize in puts

--- lib/pwrake/worker/writer.rb
+++ lib/pwrake/worker/writer.rb
@@ -5,10 +5,9 @@ module Pwrake
 
     def initialize
       @out = $stdout
-      @log = LogExecutor.instance
       @mutex = Mutex.new
       pipe_in, pipe_out = IO.pipe
-      Thread.new(pipe_in,"e:") do |pin,pre|
+      Thread.new(pipe_in,"log:") do |pin,pre|
         while s = pin.gets
           s.chomp!
           @out.puts pre+s
@@ -17,26 +16,30 @@ module Pwrake
       $stderr = pipe_out
     end
 
+    def add_logger(log)
+      @log = log
+    end
+
     def puts(s)
-      @mutex.synchronize do
-        begin
+      begin
+        @mutex.synchronize do
           @out.print s+"\n"
-          @out.flush
-        rescue Errno::EPIPE
         end
-        @log.info "<#{s}" if @log.opened?
+        @out.flush
+      rescue Errno::EPIPE
       end
+      @log.info "<#{s}" if @log
     end
 
     def print(s)
-      @mutex.synchronize do
-        begin
+      begin
+        @mutex.synchronize do
           @out.print s
-          @out.flush
-        rescue Errno::EPIPE
         end
-        @log.info "<#{s}" if @log.opened?
+        @out.flush
+      rescue Errno::EPIPE
       end
+      @log.info "<#{s}" if @log
     end
 
     def flush
