7242112ea7a7c607ab41168dc8d207b9e38d9c27
Masahiro TANAKA <masa16.tanaka@gmail.com>
2015-10-29 18:47:29 +0900

    new method: count_down, empty?; delayed empty check

--- lib/pwrake/master/fiber_pool.rb
+++ lib/pwrake/master/fiber_pool.rb
@@ -6,6 +6,7 @@ module Pwrake
       @new_fiber_block = block
       @max_fiber = max_fiber
       @n_fiber = 0
+      @count = 0
       @idle_fiber = []
       @q = []
       @new_fiber_start_time = Time.now-10
@@ -17,6 +18,7 @@ module Pwrake
 
     def enq(x)
       @q.push(x)
+      @count += 1
       if @idle_fiber.empty? and @n_fiber < @max_fiber and
           Time.now - @new_fiber_start_time > 0.1
         @idle_fiber << new_fiber
@@ -36,11 +38,25 @@ module Pwrake
       @q.shift
     end
 
+    def count_down
+      @count -= 1
+    end
+
+    def empty?
+      @count == 0
+    end
+
     def finish
       @finished = true
+      run
+    end
+
+    def run
+      r = !@idle_fiber.empty?
       while f = @idle_fiber.shift
         f.resume
       end
+      r
     end
 
     def new_fiber
