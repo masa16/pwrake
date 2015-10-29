02208a72567c3d05c178249aedb7ff44376db002
Masahiro TANAKA <masa16.tanaka@gmail.com>
2015-10-29 18:29:20 +0900

    delete branch/master_handler.rb

--- lib/pwrake/branch.rb
+++ lib/pwrake/branch.rb
@@ -5,6 +5,7 @@ require "pwrake/logger"
 require "pwrake/iomux/channel"
 require "pwrake/iomux/handler"
 require "pwrake/iomux/handler_set"
+require "pwrake/iomux/runner"
 
 require "pwrake/branch/branch_application"
 require "pwrake/branch/branch"
@@ -14,7 +15,6 @@ require "pwrake/branch/file_utils"
 require 'pwrake/branch/shell_profiler'
 require "pwrake/branch/shell"
 require "pwrake/branch/worker_communicator"
-require "pwrake/branch/master_handler"
 
 require "pwrake/option/option"
 require "pwrake/option/option_filesystem"
