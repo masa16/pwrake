cf357343abf0f6f3b21676d47dc9b66b8117e9b2
Masahiro TANAKA <masa16.tanaka@gmail.com>
2015-10-30 22:54:31 +0900

    modify exit method to catch EPIPE error

--- lib/pwrake/iomux/handler_set.rb
+++ lib/pwrake/iomux/handler_set.rb
@@ -10,8 +10,25 @@ module Pwrake
     end
 
     def exit
-      each{|hdl| hdl.put_line("exit")}
-      each{|hdl| hdl.wait_message("exited")}
+      a = []
+      each do |hdl|
+        iow = hdl.iow
+        begin
+          iow.puts "exit"
+          iow.flush
+          a << hdl
+          Log.debug "HandlerSet#exit iow=#{iow.inspect}"
+        rescue Errno::EPIPE => e
+          if Rake.application.options.debug
+            $stderr.puts "Errno::EPIPE in #{self.class}.exit iow=#{iow.inspect}"
+            $stderr.puts e.backtrace.join("\n")
+          end
+          Log.error "Errno::EPIPE in #{self.class}.exit iow=#{iow.inspect}\n"+
+            e.backtrace.join("\n")
+        end
+      end
+      a.each{|hdl| hdl.wait_message("exited")}
+      Log.debug "HandlerSet#exit end"
     end
 
   end
