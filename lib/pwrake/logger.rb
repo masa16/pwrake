84101f1537cffad3cf969d74550813028d479bec
Masahiro TANAKA <masa16.tanaka@gmail.com>
2017-02-15 20:49:16 +0900

    new method: Logger.set_logger: set @@logger from option.

--- lib/pwrake/logger.rb
+++ lib/pwrake/logger.rb
@@ -4,11 +4,37 @@ module Pwrake
 
   module Log
 
+    @@logger = nil
+
     module_function
 
+    def set_logger(option)
+      unless @@logger
+        if logdir = option['LOG_DIR']
+          ::FileUtils.mkdir_p(logdir)
+          logfile = File.join(logdir, option['LOG_FILE'])
+          @@logger = Logger.new(logfile)
+        else
+          if option['DEBUG']
+            @@logger = Logger.new($stderr)
+          else
+            @@logger = Logger.new(File::NULL)
+          end
+        end
+
+        if option['DEBUG']
+          @@logger.level = Logger::DEBUG
+        else
+          @@logger.level = Logger::INFO
+        end
+
+        at_exit{@@logger.close}
+      end
+    end
+
     def method_missing(meth_id,*args)
-      if Rake.application.logger
-        Rake.application.logger.send(meth_id,*args)
+      if @@logger
+        @@logger.send(meth_id,*args)
       end
     end
 
