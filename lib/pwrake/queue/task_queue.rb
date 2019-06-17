8c9df099fe70380629fe2662d937a59324d067ec
Masahiro TANAKA <masa16.tanaka@gmail.com>
2019-06-17 17:09:06 +0900

    change TaskQueue class: has internal queue, NonLocalityQueue or LocalityAwareQueue; gives priority to multicore tasks which use more than half cores in a node

--- lib/pwrake/queue/task_queue.rb
+++ lib/pwrake/queue/task_queue.rb
@@ -1,12 +1,14 @@
 require "pwrake/queue/queue_array"
 require "pwrake/queue/no_action_queue"
+require "pwrake/queue/non_locality_queue"
 
 module Pwrake
 
   class TaskQueue
 
-    def initialize(hostinfo_by_id, group_map=nil)
-      @enable_steal = true
+    def initialize(queue_class, hostinfo_by_id, group_map=nil)
+      @queue_class = Pwrake.const_get(queue_class)
+      @hostinfo_by_id = hostinfo_by_id
       @q_no_action = NoActionQueue.new
       @q_reserved = Hash.new
       def @q_reserved.first
@@ -15,8 +17,6 @@ module Pwrake
       def @q_reserved.last
         self[keys.last]
       end
-
-      @hostinfo_by_id = hostinfo_by_id
 
       pri = Rake.application.pwrake_options['QUEUE_PRIORITY'] || "LIHR"
       case pri
@@ -36,117 +36,103 @@ module Pwrake
         raise RuntimeError,"unknown option for QUEUE_PRIORITY: "+pri
       end
       Log.debug "@array_class=#{@array_class.inspect}"
-      init_queue(group_map)
-    end
 
-    def init_queue(group_map=nil)
-      @q_input = @array_class.new(0)
-      @q_no_input = FifoQueueArray.new
-      @n_turn = 1
-    end
+      # median number of cores
+      a = @hostinfo_by_id.map{|id,host_info| host_info.ncore}.sort
+      n = a.size
+      @n_core = (n%2==0) ? (a[n/2-1]+a[n/2])/2 : a[(n-1)/2]
 
-    attr_accessor :enable_steal
+      case Rake.application.pwrake_options['MULTICORE_TASK_PRIORITY']
+      when /^half/i, nil
+        @core_threshold = @n_core/2
+      when /^n(o(ne)?)?/i
+        @core_threshold = 0
+      else
+        raise RuntimeError,"unknown option for MULTICORE_TASK_PRIORITY: "+
+          Rake.application.pwrake_options['MULTICORE_TASK_PRIORITY']
+      end
+      @q = ((@core_threshold==0)?1:2).times.map do
+        @queue_class.new(hostinfo_by_id, @array_class, group_map)
+      end
+    end
 
     # enq
     def enq(tw)
       if tw.nil? || tw.actions.empty?
         @q_no_action.push(tw)
       else
-        enq_body(tw)
+        n = tw.use_cores
+        n += @n_core if n <= 0
+        i = (n > @core_threshold) ? 0 : 1
+        @q[i].enq_impl(tw)
       end
     end
 
-    def enq_body(tw)
-      enq_impl(tw)
-    end
-
-    def enq_impl(tw)
-      if tw.has_input_file?
-        @q_input.push(tw)
-      else
-        @q_no_input.push(tw)
+    def deq_task(&block)
+      Log.debug "deq_task from:"+(empty? ? " (empty)" : "\n#{inspect_q}")
+      deq_noaction_task(&block)
+      deq_reserve(&block)
+      @q.each do |q|
+        unless q.empty?
+          q.n_turn.times{|turn| deq_turn(q,turn,&block) }
+        end
       end
     end
 
     def deq_noaction_task(&block)
-      Log.debug "deq_noaction_task:"+(empty? ? " (empty)" : "\n#{inspect_q}")
       while tw = @q_no_action.shift
         Log.debug "deq_noaction: #{tw.name}"
         yield(tw)
       end
     end
 
-    def deq_task(&block) # locality version
-      Log.debug "deq_task from:"+(empty? ? " (empty)" : "\n#{inspect_q}")
-      queued = 0
-      @n_turn.times do |turn|
-        next if turn_empty?(turn)
-        queued += deq_turn(turn,&block)
+    def deq_reserve(&block)
+      @q_reserved.each do |host_info,tw|
+        n_idle = host_info.idle_cores || 0
+        n_core = tw.n_used_cores(host_info)
+        if n_idle >= n_core
+          @q_reserved.delete(host_info)
+          Log.debug "deq_reserve: #{tw.name} n_use_cores=#{n_core}"
+          yield(tw,host_info,n_core)
+        end
       end
     end
 
-    def deq_turn(turn,&block)
-      queued = 0
-      while true
+    def deq_turn(q,turn,&block)
+      begin
         count = 0
         @hostinfo_by_id.each_value do |host_info|
-          #Log.debug "TaskQueue#deq_turn host_info=#{host_info.name}"
-          if turn_empty?(turn)
-            return queued
-          elsif (n_idle = host_info.idle_cores) && n_idle > 0
-            if tw = @q_reserved[host_info]
-              n_use = tw.n_used_cores(host_info)
-              if n_idle < n_use
-                next
-              end
-              @q_reserved.delete(host_info)
-              Log.debug "deq_reserve: #{tw.name} n_use_cores=#{n_use}"
-            elsif tw = deq_impl(host_info,turn)
-              n_use = tw.n_used_cores(host_info)
-              if n_idle < n_use
-                @q_reserved[host_info] = tw
-                Log.debug "reserve host: #{host_info.name} for #{tw.name} (#{n_use} cores)"
-                next
-              end
-              Log.debug "deq: #{tw.name} n_use_cores=#{n_use}"
-            end
-            if tw
-              yield(tw,host_info,n_use)
+          return if q.turn_empty?(turn)
+          n_idle = host_info.idle_cores || 0
+          next if n_idle == 0 || @q_reserved[host_info]
+          if tw = q.deq_impl(host_info,turn)
+            n_core = tw.n_used_cores(host_info)
+            if n_idle >= n_core
+              Log.debug "deq: #{tw.name} n_use_cores=#{n_core}"
+              yield(tw,host_info,n_core)
               count += 1
-              queued += 1
+            else
+              @q_reserved[host_info] = tw
+              Log.debug "reserve host: #{host_info.name} for #{tw.name} (#{n_core} cores)"
             end
           end
         end
-        break if count == 0
-      end
-      queued
-    end
-
-    def turn_empty?(turn)
-      empty?
-    end
-
-    def deq_impl(host_info=nil, turn=nil)
-      @q_no_action.shift ||
-        @q_input.shift(host_info) ||
-        @q_no_input.shift(host_info)
+      end while count > 0
     end
 
     def clear
       @q_no_action.clear
       @q_reserved.clear
-      @q_input.clear
-      @q_no_input.clear
+      @q.each{|q| q.clear}
     end
 
     def empty?
       @q_no_action.empty? &&
-        @q_reserved.empty? &&
-        @q_input.empty? &&
-        @q_no_input.empty?
+      @q_reserved.empty? &&
+      @q.all?{|q| q.empty?}
     end
 
-    def _qstr(h,q)
+    def self._qstr(h,q)
       s = " #{h}: size=#{q.size} "
       case q.size
       when 0
@@ -156,19 +142,20 @@ module Pwrake
       when 2
         s << "[#{q.first.name}, #{q.last.name}]\n"
       else
-        s << "[#{q.first.name},..,#{q.last.name}]\n"
+        s << "[#{q.first.name}, .., #{q.last.name}]\n"
       end
       s
     end
 
     def inspect_q
-      _qstr("noaction",@q_no_action) +
-      _qstr("input",   @q_input) +
-      _qstr("no_input",@q_no_input) +
-      _qstr("reserved",@q_reserved)
+      [TaskQueue._qstr("noaction",@q_no_action),
+       *@q.map{|q| q.inspect_q},
+       TaskQueue._qstr("reserved",@q_reserved),
+      ].inject(&:+)
     end
 
     def drop_host(host_info)
+      @q.each{|q| q.drop_host(host_info)}
     end
 
   end
