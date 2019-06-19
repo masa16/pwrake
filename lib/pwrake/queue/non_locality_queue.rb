b3338d7d10a65d972566b6bb28cf71c4d1a1a37c
Masahiro TANAKA <masa16.tanaka@gmail.com>
2019-06-19 20:01:58 +0900

    modify NonLocalityQueue: two phase dequeue by use_cores

--- lib/pwrake/queue/non_locality_queue.rb
+++ lib/pwrake/queue/non_locality_queue.rb
@@ -6,16 +6,17 @@ module Pwrake
 
   class NonLocalityQueue
 
-    def initialize(hostinfo_by_id, array_class, group_map=nil)
+    def initialize(hostinfo_by_id, array_class, median_core, group_map=nil)
       @hostinfo_by_id = hostinfo_by_id
       @array_class = array_class
-
-      @q_input = @array_class.new(0)
-      @q_no_input = FifoQueueArray.new
-      @n_turn = 1
+      @median_core = median_core
+      @half_core = @median_core / 2
+      @q_input = @array_class.new(@median_core)
+      @q_no_input = FifoQueueArray.new(@median_core)
+      @turns = [0,1]
     end
 
-    attr_reader :n_turn
+    attr_reader :turns
 
     def enq_impl(tw)
       if tw.has_input_file?
@@ -29,9 +30,17 @@ module Pwrake
       empty?
     end
 
-    def deq_impl(host_info=nil, turn=nil)
-      @q_input.shift(host_info) ||
-      @q_no_input.shift(host_info)
+    def deq_impl(host_info, turn)
+      case turn
+      when 0
+        @q_input.shift(host_info,@half_core) ||
+          @q_no_input.shift(host_info,@half_core)
+      when 1
+        @q_input.shift(host_info,0) ||
+          @q_no_input.shift(host_info,0)
+      else
+        raise "invalid turn: #{turn}"
+      end
     end
 
     def size
